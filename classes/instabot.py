137675b359a53c3aeb139e6dbe0f2715d0d911bf
Stamatios Theodosiadis <stheodosi@csd.auth.gr>
2020-12-28 04:43:50 +0200

    new

--- classes/instabot.py
+++ classes/instabot.py
@@ -40,6 +40,7 @@ class InstaBot:
         self.chrome_options.add_argument('--headless')
         self.chrome_options.add_argument('--user-agent=' + Helper.getUserAgent())
         self.chrome_options.add_argument('--disable-gpu')
+        self.chrome_options.add_argument('--mute-audio')
         self.chrome_options.add_argument('--disable-dev-shm-usage')
         self.chrome_options.add_argument('--no-sandbox')
         self.chrome_options.add_argument('--ignore-certificate-errors')
