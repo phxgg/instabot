0364366c2f2b0f0d57fafcb75e905d47d6656b12
phxgg <stamatis.1702@gmail.com>
2021-05-12 19:54:45 +0300

    updated

--- classes/instabot.py
+++ classes/instabot.py
@@ -196,10 +196,10 @@ class InstaBot:
         # get comment textarea and click on the input box. Doing this once, for some reason did not work so i had to do this twice.
         self.logger.debug('Looking for the comment textarea & clicking on it...')
         try:
-            commentArea = self.driver.find_element_by_xpath('//textarea[contains(@aria-label,"Add a comment")]')
+            commentArea = self.driver.find_element_by_xpath('//textarea[contains(@aria-label,"Add a comment…")]')
             commentArea.click()
             sleep(5)
-            commentArea = self.driver.find_element_by_xpath('//textarea[contains(@aria-label,"Add a comment")]')
+            commentArea = self.driver.find_element_by_xpath('//textarea[contains(@aria-label,"Add a comment…")]')
             commentArea.click()
         except:
             raise Exception('Could not find the comment textarea.')
